b378ba64b23ee3ac267fa898d8f7aa97d4cb1518
liuxiangdong <92024557+liu-xiangdong@users.noreply.github.com>
2023-03-12 11:10:44 +0800

    Update 2. Add Two Numbers.py

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -8,16 +8,16 @@
 class Solution:
     def addTwoNumbers(self, l1, l2):
         head = linkedList = ListNode()
-        val = sum = 0
-        while sum or l1 or l2:
-            val = sum
+        val = up = 0
+        while up or l1 or l2:
+            val = up
             if l1:
                 val = l1.val + val
                 l1 = l1.next
             if l2:
                 val = l2.val + val
                 l2 = l2.next
-            sum = val // 10
+            up = val // 10
             val = val % 10
             linkedList.next = ListNode(val)
             linkedList = linkedList.next
